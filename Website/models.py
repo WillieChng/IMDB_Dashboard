525f7c55f334d75acf08450ed34ff11ea6ae4056
PravinG24 <pravinganesh24@gmail.com>
2024-11-25 10:35:29 +0800

    Data pre-processing

--- Website/models.py
+++ Website/models.py
@@ -5,21 +5,25 @@ from flask_login import UserMixin
 
 class Genre(db.Model):
     __tablename__ = 'genres'
+    __table_args__ = {'extend_existing': True}
     genre_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
 
 class Actor(db.Model):
     __tablename__ = 'actors'
+    __table_args__ = {'extend_existing': True}
     actor_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
 
 class Director(db.Model):
     __tablename__ = 'directors'
+    __table_args__ = {'extend_existing': True}
     director_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
 
 class Movie(db.Model):
     __tablename__ = 'movies'
+    __table_args__ = {'extend_existing': True}
     movie_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     overview = db.Column(db.Text, nullable=True)
@@ -32,57 +36,56 @@ class Movie(db.Model):
     overview_sentiment = db.Column(db.Float, nullable=True)
     all_combined_keywords = db.Column(db.Text, nullable=True)
     runtime = db.Column(db.Integer, nullable=True)
-    production_country = db.Column(db.String(100), nullable=True)
+    production_countries = db.Column(db.String(500), nullable=True)
     Star1 = db.Column(db.String(100), nullable=True)
     Star2 = db.Column(db.String(100), nullable=True)
     Star3 = db.Column(db.String(100), nullable=True)
     Star4 = db.Column(db.String(100), nullable=True)
 
-    genres = db.relationship('Genre', secondary='movie_genres', backref=db.backref('genre_movies', lazy='dynamic'))
-    actors = db.relationship('Actor', secondary='movie_actors', backref=db.backref('actor_movies', lazy='dynamic'))
-    directors = db.relationship('Director', secondary='movie_directors', backref=db.backref('director_movies', lazy='dynamic'))
+    genres = db.relationship('Genre', secondary='movie_genres', backref=db.backref('movies', lazy='dynamic'))
+    actors = db.relationship('Actor', secondary='movie_actors', backref=db.backref('movies', lazy='dynamic'))
+    directors = db.relationship('Director', secondary='movie_directors', backref=db.backref('movies', lazy='dynamic'))
 
 # Association table for movies and genres
 MovieGenre = db.Table('movie_genres',
     db.Column('movie_id', db.Integer, db.ForeignKey('movies.movie_id'), primary_key=True),
-    db.Column('genre_id', db.Integer, db.ForeignKey('genres.genre_id'), primary_key=True)
+    db.Column('genre_id', db.Integer, db.ForeignKey('genres.genre_id'), primary_key=True),
+    extend_existing=True
 )
 
 # Association table for movies and actors
 MovieActor = db.Table('movie_actors',
     db.Column('movie_id', db.Integer, db.ForeignKey('movies.movie_id'), primary_key=True),
-    db.Column('actor_id', db.Integer, db.ForeignKey('actors.actor_id'), primary_key=True)
+    db.Column('actor_id', db.Integer, db.ForeignKey('actors.actor_id'), primary_key=True),
+    extend_existing=True
 )
 
 # Association table for movies and directors
 MovieDirector = db.Table('movie_directors',
     db.Column('movie_id', db.Integer, db.ForeignKey('movies.movie_id'), primary_key=True),
-    db.Column('director_id', db.Integer, db.ForeignKey('directors.director_id'), primary_key=True)
+    db.Column('director_id', db.Integer, db.ForeignKey('directors.director_id'), primary_key=True),
+    extend_existing=True
 )
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
     user_id = db.Column(db.Integer, primary_key=True)
-    firstName = db.Column(db.String(150), nullable=False)
-    lastName = db.Column(db.String(150), nullable=False)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    profile_picture = db.Column(db.String(150), nullable=True)
-    user_favourites = db.relationship('Movie', secondary='user_favourites', backref=db.backref('favourited_by', lazy='dynamic'))
-    user_recommendations = db.relationship('Movie', secondary='user_recommendations', backref=db.backref('recommended_to', lazy='dynamic'))
+    favourites = db.relationship('Movie', secondary='user_favourites', backref=db.backref('favourited_by', lazy='dynamic'))
 
-    def get_id(self):
-        return self.user_id
-    
 # Association table for users and favorite movies
 UserFavourite = db.Table('user_favourites',
     db.Column('user_id', db.Integer, db.ForeignKey('users.user_id'), primary_key=True),
-    db.Column('movie_id', db.Integer, db.ForeignKey('movies.movie_id'), primary_key=True)
+    db.Column('movie_id', db.Integer, db.ForeignKey('movies.movie_id'), primary_key=True),
+    extend_existing=True
 )
 
 # Association table for users and recommended movies
 UserRecommendation = db.Table('user_recommendations',
     db.Column('user_id', db.Integer, db.ForeignKey('users.user_id'), primary_key=True),
-    db.Column('movie_id', db.Integer, db.ForeignKey('movies.movie_id'), primary_key=True)
+    db.Column('movie_id', db.Integer, db.ForeignKey('movies.movie_id'), primary_key=True),
+    extend_existing=True
 )
