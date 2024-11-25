b72645c27f369bddd4f7724479de836ed8f536fd
WillieChng <146805064+WillieChng@users.noreply.github.com>
2024-11-26 00:35:29 +0800

    Dashboarding

--- Website/views.py
+++ Website/views.py
@@ -164,34 +164,34 @@ def basic():
     df = get_movie_data()
     ##CHART 1: Top 10 Most Popular Movies (By vote_count and Popularity)
     df1 = df.groupby('title').agg({'vote_count': 'sum', 'popularity': 'mean'}).sort_values(by=['vote_count', 'popularity'], ascending=False).head(10)
-    fig1 = px.bar(df1, x='vote_count', y=df1.index, orientation='h')
+    fig1 = px.bar(df1, x='vote_count', y=df1.index, orientation='h', width=600, height=400)
     chart1 = pio.to_html(fig1, full_html=False)
     
     ##CHART 2: Top 10 Most Prolific Directors (By vote_count and popularity)
     df2 = df.groupby('director').agg({'vote_count': 'sum', 'popularity': 'mean'}).sort_values(by=['vote_count', 'popularity'], ascending=False).head(10).reset_index()
-    fig2 = px.treemap(df2, path=['director'], values='vote_count', color='popularity')
+    fig2 = px.treemap(df2, path=['director'], values='vote_count', color='popularity', width=600, height=400)
     chart2 = pio.to_html(fig2, full_html=False)
     
     ##CHART 3: Genre Distribution
     df3 = df.groupby('genre').size().reset_index(name="count")
-    fig3 = px.pie(df3, values="count", names="genre")
+    fig3 = px.pie(df3, values="count", names="genre", width=600, height=400)
     chart3 = pio.to_html(fig3, full_html=False) 
     
     ##CHART 4: Total Number of Movies Released Per Year
     df4 = df.groupby('release_year').size().reset_index(name="count")
-    fig4 = px.line(df4, x='release_year', y='count')
+    fig4 = px.line(df4, x='release_year', y='count', width=600, height=400)
     chart4 = pio.to_html(fig4, full_html=False)
     
     ##CHART 5: Adult vs Non-Adult Movies Count
     df5 = df.groupby('adult').size().reset_index(name="count")
-    fig4 = px.bar(df5, x='adult', y='count')
-    chart5 = pio.to_html(fig4, full_html=False)
+    fig5 = px.bar(df5, x='adult', y='count', width=600, height=400)
+    chart5 = pio.to_html(fig5, full_html=False)
     
     ##CHART 6: Most Starred Actors/Actresses (Star1, Star2, Star3, Star4)
     df6 = pd.concat([df['Star1'], df['Star2'], df['Star3'], df['Star4']]).value_counts().reset_index()
     df6.columns = ['actor', 'count']
     wordCloud = WordCloud(width=800, height=400, background_color='white').generate(' '.join(df6['actor']))
-    fig6 = px.imshow(wordCloud)
+    fig6 = px.imshow(wordCloud, width=600, height=400)
     chart6 = pio.to_html(fig6, full_html=False)
     
     return render_template("basic.html", chart1=chart1, chart2=chart2, chart3=chart3, chart4=chart4, chart5=chart5, chart6=chart6)
